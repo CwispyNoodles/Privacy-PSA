9e4ab80920706cb953e54afb2e693ff67f4cdfed
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2023-04-21 19:31:35 -0400

    installed and imported antd

--- privacy-psa/src/App.js
+++ privacy-psa/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Upload} from 'antd'
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
       <h2>Author: Bum Chan Koo</h2>
+      <Upload>
+        <p>Upload</p>
+      </Upload>
     </div>
   );
 }
