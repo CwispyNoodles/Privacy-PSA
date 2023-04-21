1ecac6e4c99de567a1f6690d4cff7d869377fcec
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2023-04-21 12:46:38 -0400

    deploy

---
+++ privacy-psa/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Author: Bum Chan Koo</h2>
+    </div>
+  );
+}
+
+export default App;
