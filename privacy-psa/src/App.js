3dc629c7a47b209a014cd8c2905eb17e55316360
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2023-04-21 20:18:53 -0400

    Update App.js

--- privacy-psa/src/App.js
+++ privacy-psa/src/App.js
@@ -2,15 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import {Upload} from 'antd'
 
+const { createRoot } = ReactDOM;
+const {  InboxOutlined  } = icons;
+const {  message, Upload  } = antd;
+const { Dragger } = Upload;
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-      <h2>Author: Bum Chan</h2>
-      <Upload>
-        <p>Upload</p>
-      </Upload>
-    </div>
+    <Dragger {...props}>
+    <p className="ant-upload-drag-icon">
+      <InboxOutlined />
+    </p>
+    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+    <p className="ant-upload-hint">
+      Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+      banned files.
+    </p>
+  </Dragger>
   );
 }
 
